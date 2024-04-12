e2f7b1e4327a3beb6b6829ed81829ebf55114ac5
namkungkang <namkung0131@naver.com>
2024-04-12 20:16:47 +0900

    .
    .

--- 실습2/second-practice/src/App.js
+++ 실습2/second-practice/src/App.js
@@ -7,10 +7,13 @@ function App() {
 
   const openModal = () => {
     setOpen(true);
+    console.log("모달이 켜짐")
   };
 
   const closeModal = () => {
     setOpen(false);
+    console.log("모달이 꺼짐")
+  
   };
 
   return (
@@ -18,6 +21,7 @@ function App() {
       <h1>안녕하세요!</h1>
       <p>내용내용내용</p>
       <button id="open" onClick={openModal}>버튼 열기</button>
+      
       {Open && <Modal closeModal={closeModal} />}
 
           </div>
