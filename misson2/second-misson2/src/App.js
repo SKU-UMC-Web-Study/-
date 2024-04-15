7330d99a3103ed9b645b3246da901881ef751576
namkungkang <namkung0131@naver.com>
2024-04-15 17:38:04 +0900

    .
    .

--- misson2/second-misson2/src/App.js
+++ misson2/second-misson2/src/App.js
@@ -1,20 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
 import { movies } from "./movieDummy";
+import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
       {movies.results.map((movie) => (
-        <Movie key={movie.id} title={movie.title} poster={movie.poster_path} />
+        <Movie key={movie.id} title={movie.title} poster={movie.poster_path} overview={movie.overview} />
       ))}
     </div>
   );
 }
 
 function Movie(props) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="movie">
+    <div className="movie" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <div className="img">
         <img
           src={`https://image.tmdb.org/t/p/w500${props.poster}`}
@@ -24,6 +26,11 @@ function Movie(props) {
       <div className="title">
         <h2>{props.title}</h2>
       </div>
+      {isHovered && (
+        <div className="details">
+          <p>{props.overview}</p>
+        </div>
+      )}
     </div>
   );
 }
